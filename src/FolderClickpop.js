8ea57d0cf5facfb3d27b57c420a09a1b32d7b50f
keerthi <jangamkeerthi26@gmail.com>
2024-02-25 16:29:47 +0530

    Add the useState hook and pass the folderName as argument in handleRenameFolder function

--- src/FolderClickpop.js
+++ src/FolderClickpop.js
@@ -30,28 +30,46 @@
 // export default FolderClickpop;
 
 import React from "react";
+import { useState } from "react";
+import "./popup.css";
 
 const FolderClickpop = ({
-  trigger,
-  setTrigger,
+  setModelAction,
   handleDeleteFolder,
   folderId,
+  folderName,
   handleRenameFolder,
 }) => {
+  const [trigger, setTrigger] = useState(true);
   return trigger ? (
     <div className="popup">
       <div className="popup-inner">
         <div className="Open">Open</div>
         <br />
-        <div className="del" onClick={() => {console.log("clicked delete"); handleDeleteFolder(folderId)}}>
+        <div
+          className="del"
+          onClick={() => {
+            console.log("clicked delete");
+            handleDeleteFolder(folderId);
+          }}
+        >
           Delete
         </div>
         <br />
-        <div className="Rename" onClick={() => handleRenameFolder(folderId)}>
+        <div
+          className="Rename"
+          onClick={() => {
+            setTrigger(false);
+            handleRenameFolder(folderId, folderName);
+          }}
+        >
           Rename
         </div>
         <br />
-        <div className="Cancel" onClick={() => setTrigger(null)}>
+        <div
+          className="Cancel"
+          onClick={() => setModelAction({ action: null, folderId: null })}
+        >
           Cancel
         </div>
         <br />
