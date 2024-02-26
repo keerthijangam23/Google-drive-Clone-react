8b9e1e3004cf3e1b1e2df916a06f228fcbd0ed6e
keerthi <jangamkeerthi26@gmail.com>
2024-02-26 16:13:42 +0530

    Remove all the commented lines from Dashboard.js file

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,105 +1,3 @@
-// import React, { useState } from "react";
-// import { FcOpenedFolder } from "react-icons/fc";
-// import Sidebar from "./sidebar";
-
-// const Context = () => {
-//   const [store, setStore] = useState([
-//     { id: 1, name: "folder1" },
-//     { id: 2, name: "folder2" }
-//   ]);
-//   const [message, setMessage] = useState("");
-//   const [Buttonpop, setButtonpop] = useState(false);
-//   const [Deletepop, setDeletepop] = useState(null);
-//   const [Renamepop, setRenamepop] = useState(null);
-//   const [renameValue, setRenameValue] = useState("");
-//   const handleChange = (event) => {
-//     setMessage(event.target.value);
-//   };
-
-//   const handleFolderCreation = (message) => {
-//     if (message !== "") {
-//       setStore((prevStore) => [
-//         ...prevStore,
-//         { id: prevStore.length + 1, name: message }
-//       ]);
-//       setMessage("");
-//       setButtonpop(false);
-//     } else {
-//       alert("Folder name should not be empty");
-//     }
-//   };
-
-//   const handleDeleteFolder = (folderId) => {
-//     setStore((prevStore) =>
-//       prevStore.filter((folder) => folder.id !== folderId)
-//     );
-//     setDeletepop(null);
-//   };
-
-//   const handleRenameFolder = (folderId) => {
-//     setDeletepop(false)
-//     setRenamepop(folderId);
-//     const folder = store.find((folder) => folder.id === folderId);
-//     setRenameValue(folder.name);
-//   };
-
-//   const handleRenameChange = (event) => {
-//     setRenameValue(event.target.value);
-//   };
-
-//   const handleRenameSubmit = (folderId) => {
-//     setStore((prevStore) =>
-//       prevStore.map((folder) =>
-//         folder.id === folderId ? { ...folder, name: renameValue } : folder
-//       )
-//     );
-//     setRenamepop(null);
-//   };
-
-//   return (
-//     <>
-//       <div className="folders">
-//         {store.map((val) => (
-//           <div
-//             className="folder-container"
-//             key={val.id}
-//             onClick={() => setDeletepop(val.id)}
-//           >
-//             <FcOpenedFolder size={65} />
-//             {Renamepop === val.id ? (
-//               <input
-//                  style={{maxWidth:"50px",maxHeight:"40px"}}
-//                 type="text"
-//                 value={renameValue}
-//                 onChange={handleRenameChange}
-//                 onBlur={() => handleRenameSubmit(val.id)}
-//                 autoFocus
-//               />
-//             ) : (
-//               <div style={{ width: "60px", height: "60px", marginLeft: "5px" }}>
-//                 {val.name}
-//               </div>
-//             )}
-//           </div>
-//         ))}
-//       </div>
-
-//       <Sidebar
-//         handleChange={handleChange}
-//         handleFolderCreation={handleFolderCreation}
-//         handleDeleteFolder={handleDeleteFolder}
-//         message={message}
-//         Buttonpop={Buttonpop}
-//         setButtonpop={setButtonpop}
-//         Deletepop={Deletepop}
-//         setDeletepop={setDeletepop}
-//         handleRenameFolder={handleRenameFolder}
-//       />
-//     </>
-//   );
-// };
-// export default Context;
-
 import React, { useEffect, useState } from "react";
 import { FcOpenedFolder } from "react-icons/fc";
 import Sidebar from "./sidebar";
@@ -115,7 +13,7 @@ const Dashboard = () => {
     action: null,
     folderId: null,
     folderName: null,
-  }); //create, delete, rename
+  });
 
   const handleFolderCreation = (message) => {
     if (message !== "") {
@@ -150,15 +48,6 @@ const Dashboard = () => {
     setFolders(Folders);
   };
 
-  // const HandleOpenFolder = (folderId) => {
-  //   // const newFolders = [
-  //   //   { id: 1, name: "f1" },
-  //   //   { id: 2, name: "f2" },
-  //   // ];
-  //   // setFolders(newFolders);
-
-  // };
-
   useEffect(() => {
     localStorage.setItem("folders", JSON.stringify(Folders));
   }, [Folders]);
@@ -192,7 +81,6 @@ const Dashboard = () => {
         deletingFolderId={modelAction.folderId}
         renameFolder={modelAction.folderName}
         handleRenameFolder={handleRenameFolder}
-        // handleOpenFolder={HandleOpenFolder}
       />
     </>
   );
