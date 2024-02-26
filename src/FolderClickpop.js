0ced86ae68240b917af8b28986d4ece0eef12043
keerthi <jangamkeerthi26@gmail.com>
2024-02-26 16:17:11 +0530

    Remove the commented lines from FolderClickpop.js file

--- src/FolderClickpop.js
+++ src/FolderClickpop.js
@@ -1,34 +1,3 @@
-// import React from "react";
-
-// const FolderClickpop = ({
-//   trigger,
-//   setTrigger,
-//   handleDeleteFolder,
-//   folderId,
-//   handleRenameFolder
-// }) => {
-//   return trigger ? (
-//     <div className="popup">
-//       <div className="popup-inner">
-//         <div className="Open">Open</div>
-//         <br />
-//         <div className="del" onClick={() => handleDeleteFolder(folderId)}>
-//           Delete
-//         </div>
-//         <br />
-//         <div className="Rename" onClick={() => handleRenameFolder(folderId)}>Rename</div>
-//         <br />
-//         <div className="Cancel" onClick={() => setTrigger(null)}>
-//           Cancel
-//         </div>
-//         <br />
-//       </div>
-//     </div>
-//   ) : null;
-// };
-
-// export default FolderClickpop;
-
 import React from "react";
 import { useState } from "react";
 import "./popup.css";
@@ -40,7 +9,6 @@ const FolderClickpop = ({
   folderId,
   folderName,
   handleRenameFolder,
-  // handleOpenFolder
 }) => {
   const [trigger, setTrigger] = useState(true);
   const navigate = useNavigate();
