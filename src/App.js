6a5be0d62cf776e34c88e80dec0135934295a1cc
keerthi <jangamkeerthi26@gmail.com>
2024-02-26 16:47:56 +0530

    import the Dashbaord.css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import OpenFolder from "./OpenFolder";
 import Dashboard from "./Dashboard";
 import "./style.css";
+import "./Dashboard.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
