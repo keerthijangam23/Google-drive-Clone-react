2623a26537ad7dea5dde5214f178f498f122d727
keerthi <jangamkeerthi26@gmail.com>
2024-02-26 16:51:36 +0530

    import FolderClickpop.css file

--- src/FolderClickpop.js
+++ src/FolderClickpop.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import "./popup.css";
+import "./FolderClickpop.css";
 import { useNavigate } from "react-router-dom";
 
 const FolderClickpop = ({
