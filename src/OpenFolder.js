6aa79f4eb5dd0aedf13fb5e39b4f88d95690b6cc
keerthi <jangamkeerthi26@gmail.com>
2024-02-26 22:44:01 +0530

    Display some initial foles on the UI and store them in localStorage

--- src/OpenFolder.js
+++ src/OpenFolder.js
@@ -1,8 +1,25 @@
 import { FaFileAlt } from "react-icons/fa";
+import { useState, useEffect } from "react";
+import "./OpenFolder.css"
 export default function OpenFolder() {
+  const [files, setfiles] = useState(
+     JSON.parse(localStorage.getItem("files")) || 
+    [
+      { id: 1, name: "file1" },
+      { id: 2, name: "file2" },
+    ]
+  );
+  useEffect(() => {
+     localStorage.setItem("files", JSON.stringify(files));
+  }, [files]);
   return (
-    <div>
-      <FaFileAlt size={60} />
+    <div className="files">
+      {files.map((val) => (
+        <div className="files-container" key={val.id}>
+          <FaFileAlt className = "file"size={65} />
+          <div className="file-name">{val.name}</div>
+        </div>
+      ))}
     </div>
   );
 }
