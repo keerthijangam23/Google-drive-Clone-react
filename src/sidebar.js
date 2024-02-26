0f70074be9a6128a2ee2c7428df31c95b60f72dd
keerthi <jangamkeerthi26@gmail.com>
2024-02-26 16:19:24 +0530

    Remove the commented lines from sidebar.js file

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,53 +1,3 @@
-// import React from "react";
-// import { NewFolder } from "./NewFolderpop";
-// import FolderClickpop from "./FolderClickpop";
-
-// const Sidebar = ({
-//   handleChange,
-//   handleFolderCreation,
-//   handleDeleteFolder,
-//   message,
-//   Buttonpop,
-//   setButtonpop,
-//   Deletepop,
-//   setDeletepop,
-//   handleRenameFolder
-// }) => {
-//   return (
-//     <>
-//       <div className="side">
-//         <img
-//           className="drive-image"
-//           src="https://tse1.mm.bing.net/th?id=OIP.nAzvj5hw-KpsOtwXu-EOFQHaGo&pid=Api&rs=1&c=1&qlt=95&w=139&h=124"
-//         />
-//         <br />
-//         <button className="item" onClick={() => setButtonpop(true)}>
-//           New
-//         </button>
-//       </div>
-
-//       <NewFolder
-//         trigger={Buttonpop}
-//         setTrigger={setButtonpop}
-//         message={message}
-//         handleChange={handleChange}
-//         handleFolderCreation={handleFolderCreation}
-//         setButtonpop={setButtonpop}
-//       />
-
-//       <FolderClickpop
-//         trigger={Deletepop}
-//         setTrigger={setDeletepop}
-//         handleDeleteFolder={handleDeleteFolder}
-//         folderId={Deletepop}
-//         handleRenameFolder={handleRenameFolder}
-//       />
-//     </>
-//   );
-// };
-
-// export default Sidebar;
-
 import React from "react";
 import FolderClickpop from "./FolderClickpop";
 import ModelPopup from "./ModelPop";
@@ -60,7 +10,7 @@ const Sidebar = ({
   deletingFolderId,
   renameFolder,
   handleRenameFolder,
-  handleOpenFolder
+  handleOpenFolder,
 }) => {
   return (
     <>
@@ -81,17 +31,15 @@ const Sidebar = ({
       {modelAction.action && (
         <ModelPopup
           handleSubmit={({ id, name }) => {
-            
             return modelAction.action === "create"
               ? handleFolderCreation(name)
-              : modelAction.folderName
-               && (handleRenameFolder(id, name),
-                setModelAction({
-                  action: null,
-                  folderId: null,
-                  folderName: null,
-                }))
-              
+              : modelAction.folderName &&
+                  (handleRenameFolder(id, name),
+                  setModelAction({
+                    action: null,
+                    folderId: null,
+                    folderName: null,
+                  }));
           }}
           handleClose={() => {
             setModelAction({ action: null, folderId: null });
